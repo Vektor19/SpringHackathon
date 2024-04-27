c56427fdc7ba4bc2ee3fb01262fbb4b9b2134c9f
Vektor19 <viktor.komissarov@knu.ua>
2024-04-28 02:49:52 +0300

    Added External Logins

--- SpringHackathon/Controllers/AccountController.cs
+++ SpringHackathon/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using SpringHackathon.Models;
 using SpringHackathon.Services;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace SpringHackathon.Controllers
 {
@@ -13,15 +16,33 @@ namespace SpringHackathon.Controllers
         private readonly SignInManager<User> _signInManager;
         private readonly IUserStore<User> _userStore;
         private readonly IUserEmailStore<User> _emailStore;
-
-        public AccountController(UserManager<User> userManager ,SignInManager<User> signInManager, IUserStore<User> userStore)
+		private readonly IAuthenticationSchemeProvider _authenticationSchemeProvider;
+		public AccountController(UserManager<User> userManager ,SignInManager<User> signInManager, IUserStore<User> userStore, IAuthenticationSchemeProvider authenticationSchemeProvider)
         {
             //_userService = new UserService(userManager, signInManager, userStore);
             _userManager = userManager;
             _signInManager = signInManager;
             _userStore = userStore;
             _emailStore = (IUserEmailStore<User>)userStore;
+			_authenticationSchemeProvider = authenticationSchemeProvider;
            
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login");
+            }
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                return View(user);
+            }
+
+            return NotFound();
+
         }
         public IActionResult Login()
         {
@@ -29,14 +50,18 @@ namespace SpringHackathon.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
-            return View();
+            ViewBag.ReturnUrl = Url.Content("~/");
+
+			return View();
         }
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel loginModel, string returnurl=null)
         {
-            if (ModelState.IsValid)
+			returnurl=returnurl ?? Url.Content("~/");
+
+			if (ModelState.IsValid)
             {
                 User appUser = await _userManager.FindByEmailAsync(loginModel.Email);
                 if (appUser != null)
@@ -87,5 +112,71 @@ namespace SpringHackathon.Controllers
             }
             return View();
         }
-    }
+		
+		public IActionResult ExternalLogin(string provider, string returnUrl = null)
+		{
+			// Перевірте, чи вказаний провайдер існує
+			var redirectUrl = Url.Action("ExternalLoginCallback", values: new { returnUrl });
+			var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+			return new ChallengeResult(provider, properties);
+		}
+
+		public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+		{
+
+			returnUrl = returnUrl ?? Url.Content("~/");
+			if (remoteError != null)
+			{
+				return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+			}
+			var info = await _signInManager.GetExternalLoginInfoAsync();
+			if (info == null)
+			{
+				return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+			}
+			var currentuser = await _userManager.FindByEmailAsync(info.Principal.FindFirstValue(ClaimTypes.Email));
+			if (currentuser != null)
+			{
+				var userWithLoginResult = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+				if (userWithLoginResult!=null)
+				{
+					var result = await _userManager.AddLoginAsync(currentuser, info);
+				}
+					// Sign in the user with this external login provider if the user already has a login.
+					/*await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+*/
+					await _signInManager.SignInAsync(currentuser, isPersistent: false, info.LoginProvider);
+					return LocalRedirect(returnUrl);
+				
+
+			}
+			if (ModelState.IsValid)
+			{
+				var user = Activator.CreateInstance<User>();
+				await _userStore.SetUserNameAsync(user, info.Principal.FindFirstValue(ClaimTypes.Email), CancellationToken.None);
+				await _emailStore.SetEmailAsync(user, info.Principal.FindFirstValue(ClaimTypes.Email), CancellationToken.None);
+				user.EmailConfirmed = true;
+
+				var result = await _userManager.CreateAsync(user);
+
+
+				if (result.Succeeded)
+				{
+					result = await _userManager.AddLoginAsync(user, info);
+					if (result.Succeeded)
+					{
+						// Sign in the user with this external login provider if the user already has a login.
+						/*await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+*/
+						await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
+						return LocalRedirect(returnUrl);
+					}
+				}
+			}
+			return RedirectToAction("Index", "Home");
+		}
+	
+	}
+
 }
+
--- SpringHackathon/Program.cs
+++ SpringHackathon/Program.cs
@@ -15,23 +15,38 @@ builder.Services.AddIdentity<User, UserRole>()
             mongoDbSettings.ConnectionString, mongoDbSettings.Name
         );
 
-    
+
 //Google auth
-/*builder.Services.AddAuthentication()
+builder.Services.AddAuthentication()
     .AddGoogle(options =>
     {
-        options.ClientId = "your-google-client-id";
-        options.ClientSecret = "your";
+        options.ClientId = builder.Configuration.GetSection("GoogleAuthSettings").GetValue<string>("ClientId");
+        options.ClientSecret = builder.Configuration.GetSection("GoogleAuthSettings").GetValue<string>("ClientSecret");
     })
-    .AddTwitter(options =>
-    {
-        options.ConsumerSecret = "your-twitter-consumer-secret";
-    })
-    .AddFacebook(options =>
-    {
-        options.ClientId = "your-facebook-client-id";
-        options.ClientSecret = "your-facebook-client-secret";
-    });*/
+     .AddTwitter(options =>
+     {
+         options.ConsumerKey = builder.Configuration.GetSection("TwitterAuthSettings").GetValue<string>("ApiKey");
+         options.ConsumerSecret = builder.Configuration.GetSection("TwitterAuthSettings").GetValue<string>("ApiKeySecret");
+         options.RetrieveUserDetails = true;
+     })
+     .AddDiscord(options =>
+     {
+         options.ClientId = builder.Configuration.GetSection("DiscordAuthSettings").GetValue<string>("ClientId");
+         options.ClientSecret = builder.Configuration.GetSection("DiscordAuthSettings").GetValue<string>("ClientSecret");
+         options.Scope.Add("identify");
+         options.Scope.Add("email");
+         options.SaveTokens = true;
+     })
+     .AddGitHub(options =>
+     {
+         options.ClientId = builder.Configuration.GetSection("GitHubAuthSettings").GetValue<string>("ClientId");
+         options.ClientSecret = builder.Configuration.GetSection("GitHubAuthSettings").GetValue<string>("ClientSecret");
+         options.Scope.Add("user:email");
+         options.Scope.Add("read:user");
+         options.SaveTokens = true;
+     });
+     
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

7bbbf2c61d8087704eadb79d4ff67df6218c1f6f
Vektor19 <viktor.komissarov@knu.ua>
2024-04-27 14:52:35 +0300

    Changed MongoDb Config

--- SpringHackathon/Settings/MongoDbConfig.cs
+++ SpringHackathon/Settings/MongoDbConfig.cs
@@ -3,8 +3,6 @@
     public class MongoDbConfig
     {
         public string Name { get; init; }
-        public string Host { get; init; }
-        public int Port { get; init; }
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString { get; init; }
     }
 }

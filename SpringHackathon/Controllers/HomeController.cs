4ddd5e900e9fecd60caee2cb2b5a752a93053b2b
Vektor19 <viktor.komissarov@knu.ua>
2024-04-28 22:58:01 +0300

    added homecontroller documentation

--- SpringHackathon/Controllers/HomeController.cs
+++ SpringHackathon/Controllers/HomeController.cs
@@ -5,47 +5,46 @@ using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pag
 using SpringHackathon.Models;
 using SpringHackathon.Services;
 using System.Diagnostics;
-using System.Xml.Linq;
 
 namespace SpringHackathon.Controllers
 {
-    
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private UserService _userService;
-        private SignInManager<User> _signInManager;
-        private readonly IUserStore<User> _userStore;
-        private readonly IUserEmailStore<User> _emailStore;
-        private RoleManager<UserRole> _roleManager;
-        private readonly UserManager<User> _userManager;
+	/// <summary>
+	/// Controller for handling home-related requests.
+	/// </summary>
+	public class HomeController : Controller
+	{
 
-		public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, SignInManager<User> signInManager, IUserStore<User> userStore, RoleManager<UserRole> roleManager)
-        {
-            _logger = logger;
-            this._userService = new UserService(userManager);
-            this._signInManager = signInManager;
-            _userStore = userStore;
-            _emailStore= (IUserEmailStore<User>)userStore;
-            _roleManager = roleManager;
-            _userManager = userManager;
-        }
+		public HomeController()
+		{
+			
+		}
 
-        public async Task<IActionResult> Index()
-        {
-
+		/// <summary>
+		/// Action method for the Index page.
+		/// </summary>
+		/// <returns>An asynchronous task that represents the operation and produces an IActionResult.</returns>
+		public async Task<IActionResult> Index()
+		{
 			return View();
-        }
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		/// <summary>
+		/// Action method for the Privacy page.
+		/// </summary>
+		/// <returns>An IActionResult representing the Privacy view.</returns>
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		/// <summary>
+		/// Action method for handling errors.
+		/// </summary>
+		/// <returns>An IActionResult representing the Error view.</returns>
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
